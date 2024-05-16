b94b6d4ca50631c16a54adf3357224a009c2a0e6
Sergei Sidorov <s.sidorov1990@mail.ru>
2024-05-16 16:18:27 +0300

    Add:The Admin Panel
    The admin panel has been added.
    Added a Currency section in the admin panel.
    Added a ExchangeRates section in the admin panel
    
    Changes to be committed:
    	modified:   exchange_rates/admin.py
    	modified:   guide_currency/admin.py

--- exchange_rates/admin.py
+++ exchange_rates/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from exchange_rates.models import ExchangeRates
+
+
+class ExchangeRatesAdmin(admin.ModelAdmin):
+
+    list_display = ["id", "currency", "date", "value"]
+    fields = ["currency", "date", "value"]
+    search_fields = ["id", "currency", "date", "value"]
+
+    list_prefetch_related = ["currency"]
+
+
+admin.site.register(ExchangeRates, ExchangeRatesAdmin)
--- guide_currency/admin.py
+++ guide_currency/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from guide_currency.models import Currency
+
+
+class CurrencyAdmin(admin.ModelAdmin):
+
+    list_display = ["id", "char_code", "name"]
+    fields = ["char_code", "name"]
+    search_fields = ["id", "char_code", "name"]
+
+
+admin.site.register(Currency, CurrencyAdmin)
